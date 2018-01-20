5a4ffce07a43d77befd4d1049605db8171cecbcf
stckwok <samuel.kwok@ntrust.com>
2018-01-19 17:04:40 -0800

    Improved output for fail test and catched error with try-except

--- test/interopUtils.py
+++ test/interopUtils.py
@@ -1,3 +1,8 @@
+import logging
+import os
+import sys
+global reporter
+
 #clientDirs = ["bucash", "abc", "xt", "classic"]
 #clientSubvers = set(["Bitcoin ABC", "Classic", "Bitcoin XT", "BUCash"])
 clientDirs = ["bucash", "abc", "xt", "bucash"]
@@ -22,3 +27,66 @@ def verifyInterconnect(nodes, clientTypes=clientSubvers):
         if notConnectedTo:
             print("Client %s is not connected to %s" % (myclient, str(notConnectedTo)))
         assert(len(notConnectedTo) == 0)
+
+class TCReporter(object):
+    """
+    Test case Reporting class keeps track of test cases that have been executed.
+    """
+    def __init__(self):
+        self.testcases = []
+
+    def add_testcase(self, testcase):
+        self.testcases.append(testcase)
+
+    def display_report(self):
+        for tc in self.testcases:
+            if (tc['status'] == "pass"):
+                msg = " ... " + "\n" + \
+                    "Name: " + tc['name'] + "\n" + \
+                    "Status: " + tc['status'] + "\n"
+            else:
+                msg = "\n" + "=================== Fail ===================" + "\n" + \
+                    "Name: " + tc['name'] + "\n" + \
+                    "File Name: " + str(tc['fname']) + "\n" + \
+                    "Line: " + str(tc['line']) + "\n" + \
+                    "Exception Type: " + str(tc['type']) + "\n" + \
+                    "Message: " + str(tc['message'])+ "\n" + \
+                    "Node 1: " + str(tc['node1']) + "\n" + \
+                    "Node 2: " + str(tc['node2']) + "\n" + \
+                    "Amount: " + str(tc['amount']) + "\n" + \
+                    "Number Signature: " + str(tc['numsig']) + "\n" + \
+                    "Status: " + tc['status'] + "\n" + \
+                    "============================================" + "\n"
+
+            logging.info(msg)
+
+reporter = TCReporter()
+
+def assert_capture(*args, **kwargs):
+    """
+    Test function decorated by this decorator will ignore with AssertionError. 
+    Instead test case results will be logged to the TCReporter.
+    """
+    def assert_decorator(func):
+        def inner(*args, **kwargs):
+            tc = {}
+            tc['name'] = func.__name__
+            tc['description'] = func.__doc__
+            try:
+                func(*args, **kwargs)
+                tc['status'] = 'pass'
+            #except AssertionError:
+            except AssertionError as e:
+                tc['status'] = 'fail'
+                messages = e.args[0]
+                tc['fname'] = messages["file_name"]
+                tc['line'] = messages["line_num"]
+                tc['type'] = messages["error_type"]
+                tc['message'] = messages["error_msg"]
+                tc['node1'] = messages["n1"]
+                tc['node2'] = messages["n2"]
+                tc['amount'] = messages["amount"]
+                tc['numsig'] = messages["numsig"]
+            reporter.add_testcase(tc)
+        return inner
+    return assert_decorator
