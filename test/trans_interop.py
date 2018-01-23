3b8a783336fc03f9a7021788bce38ba74745e7e0
stckwok <samuel.kwok@ntrust.com>
2018-01-23 07:40:37 -0800

    Improved test execution flow  (#8)
    * Add verify for main chain
    
    * Verify main chain blocklen, status, and height after sync_blocks
    
    * Update README.md file setup and build steps
    
    * Verify transaction amounts and confirmation counts between two nodes
    
    * Add check for same nodes
    
    * Added test using sendmany and createmultisig
    
    * Add folder /ramdisk to data storage
    
    * Added line for commit
    
    * Replace default tmpdir to /tmp/cashInterop instead of /ramdisk
    Allow user input to overwrite default location
    
    * Fixed identation
    
    * Rework from review comments
    
    * Removed outer parens
    
    * Removed incorrect check folder location
    
    * Updated README.md to state no space allowed in user defined location
    
    * Improved output for fail test and catched error with try-except
    
    * Added total counts of pass and fail

--- test/trans_interop.py
+++ test/trans_interop.py
@@ -12,6 +12,7 @@ import time
 import math
 import json
 import logging
+import sys
 logging.basicConfig(format='%(asctime)s.%(levelname)s: %(message)s', level=logging.INFO)
 
 from test_framework.test_framework import BitcoinTestFramework
@@ -23,14 +24,57 @@ from interopUtils import *
 num_blocks = 20
 
 
-def verify_transaction_sendmany(self, nodeTxOut, nodeTxIn, num_signature):
+def verify_transaction_sendmany(self, nodeTxOne, nodeTxTwo):
     """
     Verify the transaction of BTC using
     "sendmany" API from one node: twice to self, and twice to other node
-    "createmultisig" to create a P2SH multi-signature address
     
     See Bitcoin API JSON-RPC:
     > https://chainquery.com/bitcoin-api/sendmany
+    
+    Input:
+        nodeTxOne : node one involved in the transaction
+        nodeTxTwo : node two involved in the transaction
+        num_signature : number of signatures required
+    """
+    #logging.info("### Balance (Before) : %s" % [ x.getinfo()["balance"] for x in self.nodes])
+    send_to = { self.nodes[nodeTxOne].getnewaddress() : 0.11,
+                self.nodes[nodeTxTwo].getnewaddress() : 0.22,
+                self.nodes[nodeTxOne].getaccountaddress("from1") : 0.33,
+                self.nodes[nodeTxTwo].getaccountaddress("toself") : 0.44 }
+    txid = self.nodes[nodeTxTwo].sendmany("", send_to)
+    self.sync_all()
+    assert_array_result(self.nodes[nodeTxTwo].listtransactions(),
+                       {"category":"send","amount":Decimal("-0.11")},
+                       {"txid":txid} )
+    assert_array_result(self.nodes[nodeTxOne].listtransactions(),
+                       {"category":"receive","amount":Decimal("0.11")},
+                       {"txid":txid} )
+    assert_array_result(self.nodes[nodeTxTwo].listtransactions(),
+                       {"category":"send","amount":Decimal("-0.22")},
+                       {"txid":txid} )
+    assert_array_result(self.nodes[nodeTxTwo].listtransactions(),
+                       {"category":"receive","amount":Decimal("0.22")},
+                       {"txid":txid} )
+    assert_array_result(self.nodes[nodeTxTwo].listtransactions(),
+                       {"category":"send","amount":Decimal("-0.33")},
+                       {"txid":txid} )
+    assert_array_result(self.nodes[nodeTxOne].listtransactions(),
+                       {"category":"receive","amount":Decimal("0.33")},
+                       {"txid":txid, "account" : "from1"} )
+    assert_array_result(self.nodes[nodeTxTwo].listtransactions(),
+                       {"category":"send","amount":Decimal("-0.44")},
+                       {"txid":txid, "account" : ""} )
+    assert_array_result(self.nodes[nodeTxTwo].listtransactions(),
+                       {"category":"receive","amount":Decimal("0.44")},
+                       {"txid":txid, "account" : "toself"} )
+
+def verify_transaction_createmultisig(self, nodeTxOne, nodeTxTwo, num_signature):
+    """
+    Verify the transaction of BTC using
+    "createmultisig" to create a P2SH multi-signature address
+    
+    See Bitcoin API JSON-RPC:
     > https://chainquery.com/bitcoin-api/creatmultisig
     
     Input:
@@ -38,49 +82,17 @@ def verify_transaction_sendmany(self, nodeTxOut, nodeTxIn, num_signature):
         nodeTxIn : node Id of the TxIn (credit)
         num_signature : number of signatures required
     """
-    #logging.info("### Balance (Before) : %s" % [ x.getinfo()["balance"] for x in self.nodes])
-    send_to = { self.nodes[nodeTxOut].getnewaddress() : 0.11,
-                self.nodes[nodeTxIn].getnewaddress() : 0.22,
-                self.nodes[nodeTxOut].getaccountaddress("from1") : 0.33,
-                self.nodes[nodeTxIn].getaccountaddress("toself") : 0.44 }
-    txid = self.nodes[nodeTxIn].sendmany("", send_to)
-    self.sync_all()
-    assert_array_result(self.nodes[nodeTxIn].listtransactions(),
-                       {"category":"send","amount":Decimal("-0.11")},
-                       {"txid":txid} )
-    assert_array_result(self.nodes[nodeTxOut].listtransactions(),
-                       {"category":"receive","amount":Decimal("0.11")},
-                       {"txid":txid} )
-    assert_array_result(self.nodes[nodeTxIn].listtransactions(),
-                       {"category":"send","amount":Decimal("-0.22")},
-                       {"txid":txid} )
-    assert_array_result(self.nodes[nodeTxIn].listtransactions(),
-                       {"category":"receive","amount":Decimal("0.22")},
-                       {"txid":txid} )
-    assert_array_result(self.nodes[nodeTxIn].listtransactions(),
-                       {"category":"send","amount":Decimal("-0.33")},
-                       {"txid":txid} )
-    assert_array_result(self.nodes[nodeTxOut].listtransactions(),
-                       {"category":"receive","amount":Decimal("0.33")},
-                       {"txid":txid, "account" : "from1"} )
-    assert_array_result(self.nodes[nodeTxIn].listtransactions(),
-                       {"category":"send","amount":Decimal("-0.44")},
-                       {"txid":txid, "account" : ""} )
-    assert_array_result(self.nodes[nodeTxIn].listtransactions(),
-                       {"category":"receive","amount":Decimal("0.44")},
-                       {"txid":txid, "account" : "toself"} )
 
-    multisig = self.nodes[nodeTxIn].createmultisig(num_signature, [self.nodes[nodeTxIn].getnewaddress()])
-    self.nodes[nodeTxOut].importaddress(multisig["redeemScript"], "watchonly", False, True)
-    txid = self.nodes[nodeTxIn].sendtoaddress(multisig["address"], 0.1)
-    self.nodes[nodeTxIn].generate(1)
+    multisig = self.nodes[nodeTxTwo].createmultisig(num_signature, [self.nodes[nodeTxTwo].getnewaddress()])
+    self.nodes[nodeTxOne].importaddress(multisig["redeemScript"], "watchonly", False, True)
+    txid = self.nodes[nodeTxTwo].sendtoaddress(multisig["address"], 0.1)
+    self.nodes[nodeTxTwo].generate(1)
     self.sync_all()
-    assert(len(self.nodes[nodeTxOut].listtransactions("watchonly", 100, 0, False)) == 0)
-    assert_array_result(self.nodes[nodeTxOut].listtransactions("watchonly", 100, 0, True),
+    assert(len(self.nodes[nodeTxOne].listtransactions("watchonly", 100, 0, False)) == 0)
+    assert_array_result(self.nodes[nodeTxOne].listtransactions("watchonly", 100, 0, True),
                        {"category":"receive","amount":Decimal("0.1")},
                        {"txid":txid, "account" : "watchonly"} )
-
-
+                
 def verify_transaction_amount_confirm(self, nodeTxOut, nodeTxIn, amount):
     """
     Verify the transaction of BTC amount and confirmation from one node to the other
@@ -92,16 +104,16 @@ def verify_transaction_amount_confirm(self, nodeTxOut, nodeTxIn, amount):
     """
     debit_before = self.nodes[nodeTxOut].getinfo()["balance"]
     credit_before = self.nodes[nodeTxIn].getinfo()["balance"]
-    logging.info("### Balance of Spending Node(%s) " %debit_before)
-    logging.info("### Balance of Receiving Node(%s) " %credit_before)
+    #logging.info("### Balance of Spending Node(%s) " %debit_before)
+    #logging.info("### Balance of Receiving Node(%s) " %credit_before)
     
     txid = self.nodes[nodeTxOut].sendtoaddress(self.nodes[nodeTxIn].getnewaddress(), amount)
-    logging.info("=> txid :  %s " % txid)
+    #logging.info("=> txid :  %s " % txid)
     self.sync_all()
 
     if (nodeTxOut != nodeTxIn):
         #logging.info(self.nodes[nodeTxOut].listtransactions())
-        logging.info("### Balance (Before) : %s" % [ x.getinfo()["balance"] for x in self.nodes])  
+        #logging.info("### Balance (Before) : %s" % [ x.getinfo()["balance"] for x in self.nodes])  
         assert_array_result(self.nodes[nodeTxOut].listtransactions(),
                             {"txid":txid},
                             {"category":"send","account":"","amount":Decimal(str(-amount)),"confirmations":0})                 
@@ -119,8 +131,7 @@ def verify_transaction_amount_confirm(self, nodeTxOut, nodeTxIn, amount):
         assert_array_result(self.nodes[nodeTxIn].listtransactions(),
                             {"txid":txid},
                             {"category":"receive","account":"","amount":Decimal(str(amount)),"confirmations":1})
-        logging.info("### Balance (After): %s" % [ x.getinfo()["balance"] for x in self.nodes])
-
+        #logging.info("### Balance (After): %s" % [ x.getinfo()["balance"] for x in self.nodes])
 
         debit_after = self.nodes[nodeTxOut].getinfo()["balance"]
         credit_after = self.nodes[nodeTxIn].getinfo()["balance"]
@@ -128,7 +139,7 @@ def verify_transaction_amount_confirm(self, nodeTxOut, nodeTxIn, amount):
         logging.info("=> Balance of Receiving Node(%s) " %credit_after)
 
         # credit after transaction confirmed is increased by amount
-        assert( (credit_after - credit_before) == amount)
+        assert_equal( (credit_after - credit_before) , amount)
     else:
         # send-to-self:
         assert_array_result(self.nodes[nodeTxOut].listtransactions(),
@@ -144,7 +155,7 @@ def verify_transaction_amount_confirm(self, nodeTxOut, nodeTxIn, amount):
         logging.info("=> Balance of Receiving Node(%s) " %credit_after)
         
         # credit and debit after transaction confirmed is the same
-        assert( debit_after == credit_after )
+        assert_equal( debit_after, credit_after )
 
         self.nodes[nodeTxOut].generate(1)
         self.sync_all()
@@ -155,46 +166,78 @@ def verify_transaction_amount_confirm(self, nodeTxOut, nodeTxIn, amount):
         logging.info("=> Sync_all Balance of Receiving Node(%s) " %credit_after)
         
         # credit and debit after sync_all should still be the same
-        assert( debit_after == credit_after )
+        assert_equal( debit_after, credit_after )
         
-             
-def verify_sendto_same_node(self, amount):
+@assert_capture()
+def verify_amount_sendto_nodes(self, node1, node2, amount):
     """
-    Verify the sending and receiving nodes are the same
+    Verify amount sending and receiving between two nodes
     Input:
+        node1 : node 1 involved in the transaction
+        node2 : node 2 involved in the transaction
         amount : amount to spend in BTC
+    Exception:
+        if invalid bitcoin address is detected
     """
-    
-    logging.info("*** Same Nodes ****")
-    # send amount to self (from Node 0 to 0)
-    verify_transaction_amount_confirm(self, 0, 0, amount)
-    # send amount to self (from Node 1 to 1)
-    verify_transaction_amount_confirm(self, 1, 1, amount)
-    # send amount to self (from Node 2 to 2)
-    verify_transaction_amount_confirm(self, 2, 2, amount)
-    # send amount = 10 to self (from Node 3 to 3)
-    verify_transaction_amount_confirm(self, 3, 3, amount)
-    
- 
-def verify_sendto_different_node(self, amount):
-    """
-    Verify the sending and receiving nodes are not the same node
-    Input:
-        amount : amount to spend in BTC
-    """
-    
-    logging.info("*** Different Nodes ****")
-    # send amount from Node 0 to 1 
-    verify_transaction_amount_confirm(self, 0, 1, amount)
-    # send amount from Node 0 to 2 
-    verify_transaction_amount_confirm(self, 0, 2, amount)
-    # send amount from Node 1 to 2 
-    verify_transaction_amount_confirm(self, 1, 2, amount)
-    # send amount from Node 1 to 3 
-    verify_transaction_amount_confirm(self, 1, 3, amount)
-    # send amount from Node 2 to 3 
-    verify_transaction_amount_confirm(self, 2, 3, amount)
 
+    try:
+        verify_transaction_amount_confirm(self, node1, node2, amount)
+    except JSONRPCException as e:
+        #print('>>>> Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        if e.error['code'] == -5: # Invalid Bitcoin address
+            raise AssertionError({"file_name": fname, "line_num": exc_tb.tb_lineno, \
+                           "error_type": exc_type.__name__, "error_msg": str( e.error["message"]), \
+                           "n1" : node1, "n2" : node2, "amount" : amount, "numsig" : "N/A"})
+
+@assert_capture()
+def verify_sendmany(self, node1, node2):
+    """
+    Verify amount sending and receiving between two nodes
+    Input:
+        node1 : node 1 involved in the transaction
+        node2 : node 2 involved in the transaction
+    Exception:
+        if invalid bitcoin address is detected
+    """
+
+    try:
+        verify_transaction_sendmany(self, node1, node2)
+    except JSONRPCException as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        if e.error['code'] == -5: # Invalid Bitcoin address
+            raise AssertionError({"file_name": fname, "line_num": exc_tb.tb_lineno, \
+                           "error_type": exc_type.__name__, "error_msg": str( e.error["message"]), \
+                           "n1" : node1, "n2" : node2, "amount" : "N/A", "numsig" : "N/A"})
+
+@assert_capture()
+def verify_createmultisig(self, node1, node2, num_signature):
+    """
+    Verify the transaction of BTC using
+    "createmultisig" to create a P2SH multi-signature address
+    
+    See Bitcoin API JSON-RPC:
+    > https://chainquery.com/bitcoin-api/creatmultisig
+    
+    Input:
+        node1 : node 1 involved in the transaction
+        node2 : node 2 involved in the transaction
+        num_signature : number of signatures required
+    Exception:
+        if invalid bitcoin address is detected
+    """
+
+    try:
+        verify_transaction_createmultisig(self, node1, node2, num_signature)
+    except JSONRPCException as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        if e.error['code'] == -5: # Invalid Bitcoin address
+            raise AssertionError({"file_name": fname, "line_num": exc_tb.tb_lineno, \
+                           "error_type": exc_type.__name__, "error_msg": str( e.error["message"]), \
+                           "n1" : node1, "n2" : node2, "amount" : "N/A", "numsig" : num_signature})
 
 class CTest(BitcoinTestFramework):
     def __init__(self, build_variant, client_dirs):
@@ -223,14 +266,12 @@ class CTest(BitcoinTestFramework):
         self.sync_all()
 
     def run_test(self):
-        # #########
         logging.info("Verify that all nodes are connected")
         verifyInterconnect(self.nodes)
 
         logging.info("block count: %s" % ([ x.getblockcount() for x in self.nodes]))
         logging.info("Connection count: %s" % ([ x.getconnectioncount() for x in self.nodes]))
 
-        # #########
         logging.info("Verify that every node can produce blocks and that every other node receives them")
         for n in self.nodes:
             n.generate(num_blocks)
@@ -238,7 +279,6 @@ class CTest(BitcoinTestFramework):
 
         logging.info("block count: %s" % ([ x.getblockcount() for x in self.nodes]))
 
-        # #########
         logging.info("Verify that every node can produce P2PKH transactions and that every other node receives them")
         # first get mature coins in every client
         self.nodes[0].generate(101)
@@ -256,32 +296,33 @@ class CTest(BitcoinTestFramework):
         sync_blocks(self.nodes)
         logging.info("mempool counts: %s" % [ x.getmempoolinfo()["size"] for x in self.nodes])
 
-        logging.info("Verify transaction amounts and confirmation counts between two nodes")
-        verify_sendto_different_node(self, 10)
-        
-        logging.info("Verify transaction amounts on the same nodes")
-        verify_sendto_same_node(self, 10)
-        
-        logging.info("Verify transaction amounts and confirmation counts between two nodes")
-        verify_sendto_different_node(self, 0.5)
-        
-        logging.info("Verify transaction amounts on the same nodes")
-        verify_sendto_same_node(self, 0.5)
+        logging.info("******* Verify transaction amounts on different nodes ********* ")
+        verify_amount_sendto_nodes(self, 0, 1, 10)
+        verify_amount_sendto_nodes(self, 0, 2, 10)
+        verify_amount_sendto_nodes(self, 2, 3, 10)  #failing
+        verify_amount_sendto_nodes(self, 1, 2, 10)
+        verify_amount_sendto_nodes(self, 1, 3, 10)
+        verify_amount_sendto_nodes(self, 2, 3, 10)  #failing
+
+        logging.info("******* Verify transaction amounts on the same nodes ********* ")
+        verify_amount_sendto_nodes(self, 0, 0, 10)
+        verify_amount_sendto_nodes(self, 1, 1, 10)
+        verify_amount_sendto_nodes(self, 2, 2, 10)
+        verify_amount_sendto_nodes(self, 3, 3, 10)
         
         logging.info("Verify transaction using sendmany and createmultisig APIs : Node 0 to 1")
-        verify_transaction_sendmany(self, 0, 1, 1)
+        verify_sendmany(self, 0, 1)
+        verify_createmultisig(self, 0, 1, 1)
         
-        # Comment out for now because sometimes got : 
-        # JSONRPC error: importaddress "address" ( "label" rescan )
-        # Note: This call can take minutes to complete if rescan is true.
-#        time.sleep(10)
-#        logging.info("Verify transaction using sendmany and createmultisig APIs : Node 1 to 2")
-#        verify_transaction_sendmany(self, 1, 2, 1)
+        logging.info("Verify transaction using sendmany and createmultisig APIs : Node 1 to 2")
+        verify_sendmany(self, 1, 2)
+        verify_createmultisig(self, 1, 2, 1)
         
-#        time.sleep(10)
-#        logging.info("Verify transaction using sendmany and createmultisig APIs : Node 2 to 3")
-#        verify_transaction_sendmany(self, 2, 3, 1)
-
+        logging.info("Verify transaction using sendmany and createmultisig APIs : Node 2 to 3")
+        verify_sendmany(self, 2, 3)
+        verify_createmultisig(self, 2, 3, 1)
+        
+        reporter.display_report()
         
 def Test():
     t = CTest("debug", clientDirs)
