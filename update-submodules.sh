603d75fc5f60d9dfe45c9e3b16a4c51da7a2f641
stckwok <samuel.kwok@ntrust.com>
2018-04-10 15:11:35 -0700

    abc fallbackto github version

--- update-submodules.sh
+++ update-submodules.sh
@@ -35,26 +35,49 @@ if [ -z $XT_BRANCH ]; then
   XT_BRANCH=${3:-master}  
 fi
 
-echo " "
 echo "----------------------------------------------------- "
-echo "Check out BU: ${BU_BRANCH}"
+if [ -d "./abc" ]; then
+    echo "Removing folder abc if it exists "
+    rm -Rf abc
+fi
+
+echo "<1> Cloning ABC "
+if !(git clone https://github.com/Bitcoin-ABC/bitcoin-abc.git abc) then
+    echo "Sorry! failed to clone ABC submodule."
+    exit 1
+else
+    echo "Finished !"
+fi
+
+echo "----------------------------------------------------- "
+echo "<2> Updating submodules : BU "
+git submodule update --init --recursive bucash
+echo " "
+
+echo "----------------------------------------------------- "
+echo "<3> Updating submodules : XT "
+git submodule update --init --recursive xt
+echo " "
+
+echo "----------------------------------------------------- "
+echo "<4> Check out BU: ${BU_BRANCH}"
 cd bucash 
 pwd
 git checkout $BU_BRANCH && git pull --ff origin $BU_BRANCH 
 echo "BU is updated!"
 echo " "
 
-echo "Check out ABC: ${ABC_BRANCH}"
-cd ../abc
-pwd
-git checkout $ABC_BRANCH && git pull --ff origin $ABC_BRANCH 
-echo "ABC is updated!"
-echo " "
-
-echo "Check out XT: ${XT_BRANCH}"
+echo "<5> Check out XT: ${XT_BRANCH}"
 cd ../xt
 pwd
 git checkout $XT_BRANCH && git pull --ff origin $XT_BRANCH 
 echo "XT is updated!"
+echo " "
+
+echo "<6> Check out ABC: ${ABC_BRANCH}"
+cd ../abc
+pwd
+git checkout $ABC_BRANCH && git pull --ff origin $ABC_BRANCH
+echo "ABC is updated!"
 echo " "
 echo "----------------------------------------------------- "
